893ce21f29b43779c9b13a93f089081c7b981776
Daisy Le <daisy.le0203@gmail.com>
2022-10-27 13:40:53 -0500

    added addDepartment function

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ console.log(
      ***********************************`
 )
 
-promptUser = () => {
+const promptUser = () => {
   inquirer
     .prompt([
       {
@@ -48,25 +48,15 @@ promptUser = () => {
       const { choices } = answers
 
       if (choices === "View All Departments") {
-        showDepartments().then((department) => {
-          console.table(department[0])
-        })
-
-        promptUser()
+        showDepartments()
       }
 
       if (choices === "View All Roles") {
-        showRoles().then((role) => {
-          console.table(role[0])
-        })
-
-        promptUser()
+        showRoles()
       }
 
       if (choices === "View All Employees") {
-        showEmployees().then((employee) => {
-          console.table(employee[0])
-        })
+        showEmployees()
       }
 
       if (choices === "Add A Department") {
@@ -115,50 +105,69 @@ promptUser = () => {
     })
 }
 
-showDepartments = () => {
+const showDepartments = () => {
   return db
     .promise()
     .query("SELECT d.id, d.name AS department FROM department as d;")
+    .then((department) => {
+      console.table(department[0])
+      promptUser()
+    })
 }
 
-showRoles = () => {
+const showRoles = () => {
   return db
     .promise()
     .query(
       "SELECT r.id, r.title, d.name AS department, r.salary FROM role as r JOIN department as d ON r.department_id = d.id;"
     )
+    .then((role) => {
+      console.table(role[0])
+      promptUser()
+    })
 }
 
-showEmployees = () => {
+const showEmployees = () => {
   return db
     .promise()
     .query(
       "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
     )
+    .then((employee) => {
+      console.table(employee[0])
+      promptUser()
+    })
 }
 
-addDepartment = () => {
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "addDep",
-      message: "What would you like to do?",
-      validate: (addDep) => {
-        if (addDep) {
-          return true
-        } else {
-          console.log(` Please enter a department!`)
-        }
+const addDepartment = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "addDep",
+        message: "What department would you like to add?",
+        validate: (addDep) => {
+          if (addDep) {
+            return true
+          } else {
+            console.log(` Please enter a department!`)
+            return false
+          }
+        },
       },
-    },
-  ])
-
-  return db.promise().query("")
+    ])
+    .then((answer) => {
+      db.query(
+        "INSERT INTO department (name) VALUES (?)",
+        answer.addDep,
+        (err, result) => {
+          if (err) console.log(err)
+          console.log("Added " + answer.addDept + " to the database!")
+          showDepartments()
+        }
+      )
+    })
 }
-
-// addRole = () => {
-//   return db.promise().query("")
-// }
 
 // addEmployee = () => {
 //   return db.promise().query("")
@@ -183,3 +192,4 @@ addDepartment = () => {
 // viewBudget = () => {}
 
 // db.end()
+promptUser()
