f7999aab207dc72ed0ce14a4b54e1f23aab78d25
Daisy Le <daisy.le0203@gmail.com>
2022-10-27 22:17:32 -0500

    added connection file

---
+++ db/connection.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql2")
+const cTable = require("console.table")
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+})
+
+console.log(
+  `     ***********************************
+     *                                 *
+     *        EMPLOYEE MANAGER         *
+     *                                 *
+     ***********************************`
+)
+
+db.connect(function (err) {
+  if (err) throw err
+})
+
+module.exports = db
