c81dfa2d17130c7b9b124c5a34de725fa6c64efb
Daisy Le <daisy.le0203@gmail.com>
2022-10-29 20:02:21 -0500

    added updateEmployee function and added logo

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const inquirer = require("inquirer")
+const logo = require("asciiart-logo")
 
 require("dotenv").config()
 
 const db = require("./db/connection")
 
-const getTargetValue = (array)=>{
-  return array.map(object=>object.target)
+function init() {
+  const logoText = logo({
+    name: "Employee Manager",
+    logoColor: "bold-cyan",
+    borderColor: "bold-blue",
+  }).render()
+
+  console.log(logoText)
+
+  promptUser()
 }
 
-const getId = (array, value)=>{
-  return array.filter(object => object.target === value)[0].id
+const getTargetValue = (array) => {
+  return array.map((object) => object.target)
 }
 
+const getId = (array, value) => {
+  return array.filter((object) => object.target == value)[0].id
+}
 
 const promptUser = () => {
   inquirer
@@ -28,12 +40,6 @@ const promptUser = () => {
           "Add A Role",
           "Add An Employee",
           "Update An Employee Role",
-          "Update An Employee Manager",
-          "View Employees By Department",
-          "Delete A Department",
-          "Delete A Role",
-          "Delete An Employee",
-          "View Department Budgets",
           "Quit",
         ],
       },
@@ -67,30 +73,6 @@ const promptUser = () => {
 
       if (choices === "Update An Employee Role") {
         updateEmployee()
-      }
-
-      if (choices === "Update An Employee Manager") {
-        updateManager()
-      }
-
-      if (choices === "View Employees By Department") {
-        employeeDepartment()
-      }
-
-      if (choices === "Delete A Department") {
-        deleteDepartment()
-      }
-
-      if (choices === "Delete A Role") {
-        deleteRole()
-      }
-
-      if (choices === "Delete An Employee") {
-        deleteEmployee()
-      }
-
-      if (choices === "View Department Budgets") {
-        viewBudget()
       }
 
       if (choices === "Quit") {
@@ -163,81 +145,178 @@ const addDepartment = () => {
     })
 }
 
-const addRole = async () => { 
-  let departments = await db.promise().query("SELECT d.id, d.name AS target FROM department as d;")
-  let departmentChoices = getTargetValue(departments[0])
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "title",
-      message: "What role would you like to add?",
-      validate: (addRole) => {
-        if (addRole) {
-          return true
-        } else {
-          console.log(` Please enter a role!`)
-          return false
-        }
-      },
-    },
-    {
-      type: "input",
-      name: "salary",
-      message: "What is the salary of the role?",
-      validate: (addSalary) => {
-        if (parseInt(addSalary)) {
-          return true
-        } else {
-          console.log(` Please enter a salary!`)
-          return false
-        }
-      },
-    },
-    {
-      type: "list",
-      name: "dept",
-      message: "What department does the role belong to?",
-      choices: departmentChoices,
-    },
-  ]).then((answer) => {
+const addRole = async () => {
+  const departments = await db
+    .promise()
+    .query("SELECT d.id, d.name AS target FROM department as d;")
+  const departmentChoices = getTargetValue(departments[0])
 
-    console.log(answer)
-    let departmentId = getId(departments[0], answer.dept)
-    console.log(departmentId)
-    db.query(
-      "INSERT INTO role (title, salary, department_id) VALUES (?,?,?)",
-      [answer.title, parseInt(answer.salary), departmentId],
-      (err, result) => {
-        if (err) console.log(err)
-        console.log("Added " + answer.title + " to the database!")
-        showDepartments()
-      }
-    )
-  })
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What role would you like to add?",
+        validate: (addRole) => {
+          if (addRole) {
+            return true
+          } else {
+            console.log(` Please enter a role!`)
+            return false
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "What is the salary of the role?",
+        validate: (addSalary) => {
+          if (parseInt(addSalary)) {
+            return true
+          } else {
+            console.log(` Please enter a salary!`)
+            return false
+          }
+        },
+      },
+      {
+        type: "list",
+        name: "dept",
+        message: "What department does the role belong to?",
+        choices: departmentChoices,
+      },
+    ])
+    .then((answer) => {
+      const departmentId = getId(departments[0], answer.dept)
+      db.query(
+        "INSERT INTO role (title, salary, department_id) VALUES (?,?,?)",
+        [answer.title, parseInt(answer.salary), departmentId],
+        (err, result) => {
+          if (err) console.log(err)
+          console.log("Added " + answer.title + " to the database!")
+          showRoles()
+        }
+      )
+    })
+}
+
+const addEmployee = async () => {
+  let roles = await db.promise().query("SELECT role.id, role.title FROM role")
+  roles = roles[0]
+  const roleChoices = roles.map(({ id, title }) => ({
+    name: `${title}`,
+    value: id,
+  }))
+
+  let employees = await db.promise().query("SELECT * FROM employee")
+  employees = employees[0]
+  const managerChoices = employees.map(({ id, first_name, last_name }) => ({
+    name: `${first_name} ${last_name}`,
+    value: id,
+  }))
+  managerChoices.unshift({ name: "None", value: null })
+
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "What is the employee's first name?",
+        validate: (addFirst) => {
+          if (addFirst) {
+            return true
+          } else {
+            console.log(` Please enter a first name!`)
+            return false
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "What is the employee's last name?",
+        validate: (addLast) => {
+          if (addLast) {
+            return true
+          } else {
+            console.log(` Please enter a last name!`)
+            return false
+          }
+        },
+      },
+      {
+        type: "list",
+        name: "title",
+        message: "What is the employee's role?",
+        choices: roleChoices,
+      },
+      {
+        type: "list",
+        name: "manager",
+        message: "Who is the employee's manager?",
+        choices: managerChoices,
+      },
+    ])
+    .then((answer) => {
+      console.log(answer)
+      db.query(
+        "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)",
+        [answer.firstName, answer.lastName, answer.title , answer.manager],
+        (err, result) => {
+          if (err) console.log(err)
+          console.log(
+            `Added ${answer.firstName} ${answer.lastName} to the database!`
+          )
+          showEmployees()
+        }
+      )
+    })
 
 }
 
-// addEmployee = () => {
-//   return db.promise().query("")
-// }
+const updateEmployee = async () => {
+  const employees = await db.promise().query("SELECT * FROM employee;")
+  const employeeChoices = employees[0].map(({ id, first_name, last_name }) => ({
+    name: `${first_name} ${last_name}`,
+    value: id,
+  }))
+  console.log(employeeChoices);
 
-// updateEmployee = () => {
-//   return db.promise().query("")
-// }
+  let roles = await db.promise().query("SELECT role.id, role.title FROM role")
+  roles = roles[0]
+  const roleChoices = roles.map(({ id, title }) => ({
+    name: `${title}`,
+    value: id,
+  }))
+  console.log(roleChoices);
 
-// updateManager = () => {
-//   return db.promise().query("")
-// }
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "employee",
+        message: "Which employee would you like to update?",
+        choices: employeeChoices,
+      },
+      {
+        type: "list",
+        name: "role",
+        message: "What is the employee's new role?",
+        choices: roleChoices,
+      },
+    ])
+    .then((answer) => {
+      console.log(answer);
+      console.log(answer);
+      db.query(
+        "UPDATE employee SET role_id = ? WHERE id = ?;", [answer.role, answer.employee],
+        (err, result) => {
+          if (err) console.log(err)
+          console.log(`Employee has been updated!`)
+          showEmployees()
+        }
+      )
+    })
+}
 
-// employeeDepartment = () => {}
-
-// deleteDepartment = () => {}
-
-// deleteRole = () => {}
-
-// deleteEmployee = () => {}
-
-// viewBudget = () => {}
-
-// db.end()
-promptUser()
+init()
